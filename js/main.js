bdf63e09e5ed69174ab26d552347cdcd983d10f2
Byron <jamesblocker81@gmail.com>
2018-01-30 15:31:01 -0800

    add in button functionality

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@
 // Write a function(s) for what should happen when you click on any digit
 
 // Write a function(s) for what should happen when you click on any operator
+
+//button buttons
+var buttons = document.querySelectorAll('.btn');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', checkInner);
+}
+
+function checkInner() {
+    var input = this.innerText;
+    return input;
+}
+
+//readout display
+
+var display = document.querySelector('#readout');
+
+
+
+
+
+
+add(num1, num2) {
+    return num1 + num2;
+}
